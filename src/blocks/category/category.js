4da846ce28878fec8160eff3a1ec29b4cfc874a9
Shakhrom Mukumov <leon9208@mail.ru>
2023-04-11 20:23:15 +0300

    category nav select

--- src/blocks/category/category.js
+++ src/blocks/category/category.js
@@ -1,15 +1,19 @@
 //category
 const categoryNav = document.querySelectorAll('.category-nav__input')
 const catGames = document.querySelectorAll('.category-grid .game[data-category]')
+const catGamesTitle = document.querySelector('.category-body__title')
 
 const filterGames = (category) => {
-	catGames.forEach(games => "all" === category || games.dataset.category === category ? games.style.display="block" : games.style.display="none")
+	catGames.forEach(games => {
+		catGamesTitle ? catGamesTitle.textContent = category.nextElementSibling.querySelector('.category-nav__title').textContent : ''
+		"all" === category.value || games.dataset.category === category.value ? games.style.display="block" : games.style.display="none"
+	})
 }
 
 if (categoryNav.length > 0) {
 	categoryNav.forEach(catNav => {
 		catNav.addEventListener('change', () => {
-			filterGames(catNav.value)
+			filterGames(catNav)
 		})
 	})
 
