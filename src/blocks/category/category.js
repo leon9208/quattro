b3c6ca35f58f31616e2615dd53a6cd9841b61ebd
Shakhrom Mukumov <leon9208@mail.ru>
2023-04-11 13:38:05 +0300

    Category filter

--- src/blocks/category/category.js
+++ src/blocks/category/category.js
@@ -1,1 +1,16 @@
 //category
+const categoryNav = document.querySelectorAll('.category-nav__input')
+const catGames = document.querySelectorAll('.category-grid .game[data-category]')
+
+const filterGames = (category) => {
+	catGames.forEach(games => "all" === category || games.dataset.category === category ? games.style.display="block" : games.style.display="none")
+}
+
+if (categoryNav.length > 0) {
+	categoryNav.forEach(catNav => {
+		catNav.addEventListener('change', () => {
+			filterGames(catNav.value)
+		})
+	})
+
+}
