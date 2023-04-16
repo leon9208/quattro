af3a45416ecc72f7170f00317e295b5fcb8eeb19
Shakhrom Mukumov <leon9208@mail.ru>
2023-04-16 05:22:10 +0300

    удаление кастомных стилей для модального окна

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -1,3 +1,30 @@
-//modal
+// //modal
+// const htmlTag = document.documentElement
 // var Scrollbar = window.Scrollbar;
-// Scrollbar.initAll()
+// Scrollbar.initAll();
+
+// const closeModal = () => {
+// 	htmlTag.classList.remove('_overflow-hidden')
+// 	if(document.querySelector('.modal').classList.contains('isopen')) {
+// 		document.querySelector('.modal.isopen').classList.remove('isopen')
+// 	}
+// }
+
+// const openModal = (selector) => {
+// 	let modalSelector = document.querySelector(selector)
+// 	if(modalSelector != null) {
+// 		closeModal()
+// 		htmlTag.classList.add('_overflow-hidden')
+// 		modalSelector.classList.add('isopen')
+// 	}
+// }
+
+// const closeModalBtn = document.querySelectorAll('[data-modal-close]')
+
+// if (closeModalBtn.length > 0) {
+// 	closeModalBtn.addEventListener('click', closeModal)
+// }
+
+// setTimeout(
+// 	openModal('#search-modal')
+// , 3000)
