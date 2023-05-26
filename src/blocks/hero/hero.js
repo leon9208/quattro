2cd29c15f89b36030e2d70696adba2dba22ea813
Shakhrom <leon9208@mail.ru>
2023-05-26 18:35:24 +0300

    hero slider

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,1 +1,30 @@
 //hero
+const promoSelect = document.querySelectorAll('.promo-select')
+
+if (promoSelect) {
+	promoSelect.forEach(prS => {
+		prS.addEventListener('click', () => {
+			prS.classList.add('isopen')
+			hideOnClickOutside(prS)
+		});
+	
+	
+		prS.querySelectorAll('.promo-select__item').forEach(el => {
+			el.addEventListener('click', (e) => {
+				prS.querySelectorAll('.promo-select__item').forEach(el => el.classList.remove('isactive'))
+				e.stopPropagation()
+				el.classList.add('isactive')
+				prS.classList.remove('isopen')
+			})
+		})
+	})
+};
+
+const heroSlider = new Swiper('.hero-slider', {
+  speed: 400,
+  pagination: {
+    el: '.hero-slider .swiper-pagination',
+		clickable: true
+    // type: 'bullets',
+  },
+});
