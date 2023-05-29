7a8c7a637f0595b69dd867f05b2c8a9e3aac39cb
Shakhrom <leon9208@mail.ru>
2023-05-29 15:51:40 +0300

    cagetory-games fadeIn animation

--- src/blocks/category/category.js
+++ src/blocks/category/category.js
@@ -1,3 +1,22 @@
+const fadeIn = (el, timeout, display) => {
+  el.style.opacity = 0;
+  el.style.display = display || 'block';
+  el.style.transition = `opacity ${timeout}ms`;
+  setTimeout(() => {
+    el.style.opacity = 1;
+  }, 10);
+};
+
+const fadeOut = (el, timeout) => {
+  el.style.opacity = 1;
+  el.style.transition = `opacity ${timeout}ms`;
+  el.style.opacity = 0;
+
+  setTimeout(() => {
+    el.style.display = 'none';
+  }, timeout);
+};
+
 //category filter
 const categoryNav = document.querySelectorAll('.category-nav__input')
 const catGames = document.querySelectorAll('.category-grid .game[data-category]')
@@ -6,7 +25,7 @@ const catGamesTitle = document.querySelector('.category-body__title')
 const filterGames = (category) => {
 	catGames.forEach(games => {
 		catGamesTitle ? catGamesTitle.textContent = category.nextElementSibling.querySelector('.category-nav__title').textContent : ''
-		"all" === category.value || games.dataset.category === category.value ? games.style.display="block" : games.style.display="none"
+		"all" === category.value || games.dataset.category === category.value ? fadeIn(games, 1000, 'block') : games.style.display="none"
 	})
 }
 
