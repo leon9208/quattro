d7b455fb6d991a22a398224a3c486ed34065647e
Shakhrom <leon9208@mail.ru>
2023-06-18 18:10:06 +0300

    cookie для темы сайта

--- src/blocks/nav/nav.js
+++ src/blocks/nav/nav.js
@@ -1,7 +1,29 @@
-const themeBtn = document.querySelectorAll('.theme-switcher__input')
-const doc = document.documentElement
+//theme switcher
+const themeBtn = document.querySelectorAll('.theme-switcher__input');
 themeBtn.forEach(thBtn => {
 	thBtn.addEventListener('change', () => {
-		thBtn.checked ? doc.classList.add('_dark-theme') : doc.classList.remove('_dark-theme')
+		if(thBtn.checked) {
+			document.documentElement.classList.add('_dark-theme')
+			themeBtn.forEach(thBtn => thBtn.checked = true)
+			setCookie('theme', 'dark', {secure: true, 'max-age': 99999999});
+		} else {
+			document.documentElement.classList.remove('_dark-theme')
+			themeBtn.forEach(thBtn => thBtn.checked = false)
+			deleteCookie('theme')
+		}
 	})
-})
+});
+
+const themeSwitcher = () => {
+	if (getCookie('theme')) {
+		document.documentElement.classList.add('_dark-theme')
+		themeBtn.forEach(thBtn => thBtn.checked = true)
+	} else {
+		document.documentElement.classList.remove('_dark-theme')
+		themeBtn.forEach(thBtn => thBtn.checked = false)
+	}
+};
+
+themeSwitcher();
+
+//end theme switcher
